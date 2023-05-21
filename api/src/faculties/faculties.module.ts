c261492afbdac60f87ce13eeaa8b96d18ed75052
Musanzi <musanziw.m@gmail.com>
2023-05-21 23:17:12 +0200

    [ADD] Create faculties module skeleton

---
+++ api/src/faculties/faculties.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FacultiesService } from './faculties.service';
+import { FacultiesController } from './faculties.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Faculty} from "./entities/faculty.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Faculty])],
+  controllers: [FacultiesController],
+  providers: [FacultiesService]
+})
+export class FacultiesModule {}
