a3944b851539dd01a22f83173da08330acd64d01
Musanzi <musanziw.m@gmail.com>
2023-05-21 23:21:28 +0200

    [UPDATE]

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,44 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {UsersModule} from './users/users.module';
+import {FacultiesModule} from './faculties/faculties.module';
+import {FieldsModule} from './fields/fields.module';
+import {GradesModule} from './grades/grades.module';
+import {StudientsModule} from './studients/studients.module';
+import {PromotionsModule} from './promotions/promotions.module';
+import {CoursesModule} from './courses/courses.module';
+import {RolesModule} from './roles/roles.module';
+import {PeriodsModule} from './periods/periods.module';
+import {SessionsModule} from './sessions/sessions.module';
+import {ReportsModule} from './reports/reports.module';
+import { AuthModule } from './auth/auth.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        AuthModule,
+        UsersModule,
+        FacultiesModule,
+        FieldsModule,
+        GradesModule,
+        StudientsModule,
+        PromotionsModule,
+        CoursesModule,
+        RolesModule,
+        PeriodsModule,
+        SessionsModule,
+        ReportsModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'studients-deliberation',
+            entities: ["dist/**/*.entity{.ts,.js}"],
+            synchronize: true,
+        }),
+    ]
 })
-export class AppModule {}
+
+export class AppModule {
+}
