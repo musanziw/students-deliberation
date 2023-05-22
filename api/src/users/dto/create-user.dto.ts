dfa2fe31316bad0fcf64234a6a1ac0c81bf24bb6
Musanzi <musanziw.m@gmail.com>
2023-05-22 15:49:10 +0200

    [CREATE] Add user DTOs.

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import {Column, PrimaryGeneratedColumn} from "typeorm";
+import {IsEmail, IsString} from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    firstname
+
+    @IsString()
+    lastname
+
+    @IsString()
+    name
+
+    @IsEmail()
+    email
+
+    @IsString()
+    password
+}
