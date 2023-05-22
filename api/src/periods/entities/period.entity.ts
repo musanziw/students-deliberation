eacc5d6488fc304fd7c04517ceea1c12fd3e5fcb
Musanzi <musanziw.m@gmail.com>
2023-05-22 13:28:14 +0200

    [CREATE] Create period entity.

--- api/src/periods/entities/period.entity.ts
+++ api/src/periods/entities/period.entity.ts
@@ -1,1 +1,16 @@
-export class Period {}
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Period {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    semester: number
+
+    @Column()
+    start: Date
+
+    @Column()
+    end: Date
+}
