297a591364da83e829a33b532d7e64c30cf60b88
Musanzi <musanziw.m@gmail.com>
2023-05-22 13:30:12 +0200

    [CREATE] Create user entity.

--- api/src/users/entities/user.entity.ts
+++ api/src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstname: string
+
+    @Column()
+    lastname: string
+
+    @Column()
+    name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+}
