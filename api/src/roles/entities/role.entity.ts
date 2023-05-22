613eb3a1f5f0e09530ebf557ef681919c5d5e91b
Musanzi <musanziw.m@gmail.com>
2023-05-22 13:28:46 +0200

    [CREATE] Create role entity.

--- api/src/roles/entities/role.entity.ts
+++ api/src/roles/entities/role.entity.ts
@@ -1,1 +1,10 @@
-export class Role {}
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Role {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+}
