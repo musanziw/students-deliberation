bb83eae56a2f249186400b10406ad6d07d322e11
Musanzi <musanziw.m@gmail.com>
2023-05-22 15:48:47 +0200

    [CREATE] Add student DTOs.

--- api/src/studients/dto/update-studient.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStudientDto } from './create-studient.dto';
-
-export class UpdateStudientDto extends PartialType(CreateStudientDto) {}
--- api/src/studients/studients.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StudientsService } from './studients.service';
-import { StudientsController } from './studients.controller';
-
-@Module({
-  controllers: [StudientsController],
-  providers: [StudientsService]
-})
-export class StudientsModule {}
