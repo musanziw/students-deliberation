77256ebd9668d1fdd6270d2e6c8e490aa718181d
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-24 17:27:52 +0200

    [ADD] Add necessary fields.

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,5 +1,18 @@
-import {PartialType} from '@nestjs/mapped-types';
-import {CreateUserDto} from './create-user.dto';
+import { IsArray, IsString } from 'class-validator';
+import { Role } from '../../roles/entities/role.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
+  @IsString({ message: 'Le prénom doit être une chaîne de caractères.' })
+  firstname: string;
+
+  @IsString({ message: 'Le nom doit être une chaîne de caractères.' })
+  lastname: string;
+
+  @IsString({
+    message: "Le nom d'utilisateur doit être une chaîne de caractères.",
+  })
+  name: string;
+
+  @IsArray({ message: 'Les rôles doivent être un tableau de rôles.' })
+  roles: Role[];
 }
