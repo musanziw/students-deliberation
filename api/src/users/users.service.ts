e547248bcdc969c71aaf1449a331401fd625eac9
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-24 17:08:53 +0200

    [ADD] User CRUD.

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,26 +1,119 @@
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { password } = createUserDto;
+    const salt: string = await bcrypt.genSalt(10);
+    const hashedPassword: string = await bcrypt.hash(password, salt);
+    try {
+      await this.userRepository.save({
+        ...createUserDto,
+        password: hashedPassword,
+      });
+      return {
+        status: HttpStatus.CREATED,
+        message: "L'utilisateur a bien été créé.",
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: "L'utilisateur n'a pas pu être créé.",
+      };
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    try {
+      const users: User[] = await this.userRepository.find({
+        relations: ['roles'],
+        order: { id: 'ASC' },
+      });
+      return {
+        status: HttpStatus.OK,
+        users,
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: "Les utilisateurs n'ont pas pu être récupérés.",
+      };
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const user: User = await this.userRepository.findOneOrFail({
+        where: { id },
+        relations: ['roles'],
+      });
+      return {
+        status: HttpStatus.OK,
+        user,
+      };
+    } catch {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: "L'utilisateur n'a pas pu être récupéré.",
+      };
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const { password } = updateUserDto;
+    const salt: string = await bcrypt.genSalt(10);
+    const hashedPassword: string = await bcrypt.hash(password, salt);
+    try {
+      const user: User = await this.userRepository.findOneOrFail({
+        where: { id },
+      });
+      const data = {
+        ...updateUserDto,
+        password: hashedPassword,
+      };
+      console.log(data, user);
+      await this.userRepository.save({
+        ...user,
+        ...data,
+      });
+      return {
+        status: HttpStatus.OK,
+        message: "L'utilisateur a bien été mis à jour.",
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: "L'utilisateur n'a pas pu être mis à jour.",
+      };
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    try {
+      const user: User = await this.userRepository.findOneOrFail({
+        where: { id },
+      });
+      await this.userRepository.remove(user);
+      return {
+        status: HttpStatus.OK,
+        message: "L'utilisateur a bien été supprimé.",
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: "L'utilisateur n'a pas pu être supprimé.",
+      };
+    }
   }
 }
