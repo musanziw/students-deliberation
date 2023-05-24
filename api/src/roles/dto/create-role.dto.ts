e1e935e54982290228a4affb7e36303b83142a1e
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-24 17:02:48 +0200

    [ADD] Add custom error messages.

--- api/src/roles/dto/create-role.dto.ts
+++ api/src/roles/dto/create-role.dto.ts
@@ -1,6 +1,6 @@
 import { IsString } from 'class-validator';
 
 export class CreateRoleDto {
-  @IsString()
+  @IsString({ message: 'Le nom du rôle doit être une chaîne de caractères.' })
   name;
 }
--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,18 +1,27 @@
-import {IsEmail, IsString} from "class-validator";
+import { IsArray, IsEmail, IsString } from 'class-validator';
+import { Role } from '../../roles/entities/role.entity';
 
 export class CreateUserDto {
-    @IsString()
-    firstname
+  @IsString({ message: 'Le prénom doit être une chaîne de caractères.' })
+  firstname: string;
 
-    @IsString()
-    lastname
+  @IsString({ message: 'Le nom doit être une chaîne de caractères.' })
+  lastname: string;
 
-    @IsString()
-    name
+  @IsString({
+    message: "Le nom d'utilisateur doit être une chaîne de caractères.",
+  })
+  name: string;
 
-    @IsEmail()
-    email
+  @IsEmail(
+    {},
+    { message: "L'adresse email doit être une adresse email valide." },
+  )
+  email: string;
 
-    @IsString()
-    password
+  @IsString({ message: 'Le mot de passe doit être une chaîne de caractères.' })
+  password: string;
+
+  @IsArray({ message: 'Les rôles doivent être un tableau de rôles.' })
+  roles: Role[];
 }
