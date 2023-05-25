f600a7934214a555bd46c84cef8f44c1fa8a0bbe
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-25 17:32:14 +0200

    [REFACTOR] Reformat.

--- api/src/students/entities/studient.entity.ts
+++ api/src/students/entities/studient.entity.ts
@@ -1,38 +1,48 @@
-import {Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {Promotion} from "../../promotions/entities/promotion.entity";
-import {Course} from "../../courses/entities/course.entity";
-import {Report} from "../../reports/entities/report.entity";
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Promotion } from '../../promotions/entities/promotion.entity';
+import { Course } from '../../courses/entities/course.entity';
+import { Report } from '../../reports/entities/report.entity';
 
 @Entity()
 export class Student {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    firstname: string
+  @Column()
+  firstname: string;
 
-    @Column()
-    lastname: string
+  @Column()
+  lastname: string;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
-    @Column()
-    email: string
+  @Column()
+  email: string;
 
-    @Column()
-    personal_number: string
+  @Column()
+  personal_number: string;
 
-    @Column()
-    as_complementary_course: boolean
+  @Column()
+  as_complementary_course: boolean;
 
-    @ManyToOne(() => Promotion, (promotion) => promotion.students)
-    promotion: Promotion
+  @ManyToOne(() => Promotion, (promotion) => promotion.students)
+  promotion: Promotion;
 
-    @ManyToMany(() => Course)
-    @JoinTable()
-    courses: Course[]
+  @ManyToMany(() => Course)
+  @JoinTable({
+    name: 'student_courses',
+  })
+  courses: Course[];
 
-    @OneToMany(() => Report, (report) => report.student)
-    reports: Report[]
+  @OneToMany(() => Report, (report) => report.student)
+  reports: Report[];
 }
