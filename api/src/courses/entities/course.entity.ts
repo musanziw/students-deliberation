8866e5125ec389eea10701f3f70b6b9b56cf364e
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-26 00:05:07 +0200

    [UPDATE] Add teacher instead of generic user.

--- api/src/courses/entities/course.entity.ts
+++ api/src/courses/entities/course.entity.ts
@@ -1,32 +1,41 @@
-import {Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {Promotion} from "../../promotions/entities/promotion.entity";
-import {User} from "../../users/entities/user.entity";
-import {Period} from "../../periods/entities/period.entity";
-import {Grade} from "../../grades/entities/grade.entity";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Promotion } from '../../promotions/entities/promotion.entity';
+import { User } from '../../users/entities/user.entity';
+import { Period } from '../../periods/entities/period.entity';
+import { Grade } from '../../grades/entities/grade.entity';
 
 @Entity()
 export class Course {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
-    @Column()
-    hours: number
+  @Column()
+  hours: number;
 
-    @Column()
-    credit: number
+  @Column()
+  credit: number;
 
-    @ManyToOne(() => Promotion, (promotion) => promotion.courses)
-    promotion: Promotion
+  @ManyToOne(() => Promotion, (promotion) => promotion.courses)
+  promotion: Promotion;
 
-    @ManyToOne(() => User, (user) => user.courses)
-    user: User
+  @ManyToOne(() => User, (user) => user.courses)
+  teacher: User;
 
-    @ManyToOne(() => Period, (period) => period.courses)
-    period: Period
+  @ManyToOne(() => Period, (period) => period.courses)
+  period: Period;
 
-    @OneToMany(() => Grade, (grade) => grade.course, {cascade: true, onDelete: 'CASCADE'})
-    grades: Grade[]
+  @OneToMany(() => Grade, (grade) => grade.course, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  grades: Grade[];
 }
