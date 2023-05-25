74bb750eca38123904b65a0c59bd6f3b40f50543
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-25 17:29:01 +0200

    [ADD] Import User entity to allow dependency injection in the service.

--- api/src/faculties/faculties.module.ts
+++ api/src/faculties/faculties.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FacultiesService } from './faculties.service';
 import { FacultiesController } from './faculties.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Faculty } from './entities/faculty.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Faculty])],
   controllers: [FacultiesController],
-  providers: [FacultiesService]
+  providers: [FacultiesService],
 })
 export class FacultiesModule {}
