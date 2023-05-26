fc562fea2670005619ccbf29fee7fec3c8dbd168
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-27 00:20:56 +0200

    [ADD] Add CURD for period.

--- api/src/periods/periods.service.ts
+++ api/src/periods/periods.service.ts
@@ -1,26 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePeriodDto } from './dto/create-period.dto';
 import { UpdatePeriodDto } from './dto/update-period.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Period } from './entities/period.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PeriodsService {
-  create(createPeriodDto: CreatePeriodDto) {
-    return 'This action adds a new period';
+  constructor(
+    @InjectRepository(Period)
+    private periodRepository: Repository<Period>,
+  ) {}
+
+  async create(createPeriodDto: CreatePeriodDto) {
+    try {
+      await this.periodRepository.save(createPeriodDto);
+      return {
+        status: HttpStatus.CREATED,
+        message: 'Période créée avec succès.',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de créer la période.',
+      };
+    }
   }
 
-  findAll() {
-    return `This action returns all periods`;
+  async findAll() {
+    try {
+      const periods = await this.periodRepository.find({
+        order: { id: 'ASC' },
+      });
+      return {
+        status: HttpStatus.OK,
+        message: 'Périodes récupérées avec succès.',
+        periods,
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de récupérer les périodes.',
+      };
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} period`;
+  async findOne(id: number) {
+    try {
+      const period = await this.periodRepository.findOneOrFail({
+        where: { id },
+      });
+      return {
+        status: HttpStatus.OK,
+        message: 'Période récupérée avec succès.',
+        period,
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de récupérer la période.',
+      };
+    }
   }
 
-  update(id: number, updatePeriodDto: UpdatePeriodDto) {
-    return `This action updates a #${id} period`;
+  async update(id: number, updatePeriodDto: UpdatePeriodDto) {
+    try {
+      await this.periodRepository.update(id, updatePeriodDto);
+      return {
+        status: HttpStatus.OK,
+        message: 'Période modifiée avec succès.',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de modifier la période.',
+      };
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} period`;
+  async remove(id: number) {
+    try {
+      await this.periodRepository.delete({ id });
+      return {
+        status: HttpStatus.OK,
+        message: 'Période supprimée avec succès.',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de supprimer la période.',
+      };
+    }
   }
 }
