6f6a981dd8472fdc9c648859017c7fed5e55998d
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-27 00:18:21 +0200

    [ADD] Add CRUD course.

--- api/src/courses/courses.service.ts
+++ api/src/courses/courses.service.ts
@@ -1,26 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from './entities/course.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoursesService {
-  create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+  constructor(
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>,
+  ) {}
+
+  async create(createCourseDto: CreateCourseDto) {
+    try {
+      await this.courseRepository.save(createCourseDto);
+      return {
+        status: 201,
+        message: 'Course created successfully',
+      };
+    } catch {
+      return {
+        status: 400,
+        message: 'Course created failed',
+      };
+    }
   }
 
-  findAll() {
-    return `This action returns all courses`;
+  async findAll() {
+    try {
+      const courses: Course[] = await this.courseRepository.find({
+        order: { id: 'ASC' },
+      });
+      return {
+        status: HttpStatus.OK,
+        courses,
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Aucun cours trouvé.',
+      };
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} course`;
+  async findOne(id: number) {
+    try {
+      const course: Course = await this.courseRepository.findOneOrFail({
+        where: { id },
+      });
+      return {
+        status: HttpStatus.OK,
+        course,
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de trouver le cours.',
+      };
+    }
   }
 
-  update(id: number, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
+  async update(id: number, updateCourseDto: UpdateCourseDto) {
+    try {
+      const course: Course = await this.courseRepository.findOneOrFail({
+        where: { id },
+      });
+      await this.courseRepository.save({
+        ...course,
+        ...updateCourseDto,
+      });
+      return {
+        status: HttpStatus.OK,
+        message: 'Le cours a été mis à jour avec succès.',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de mettre à jour le cours.',
+      };
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} course`;
+  async remove(id: number) {
+    try {
+      await this.courseRepository.delete({ id });
+      return {
+        status: HttpStatus.OK,
+        message: 'Le cours a été supprimé avec succès.',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Impossible de supprimer le cours.',
+      };
+    }
   }
 }
