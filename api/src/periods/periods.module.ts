39c974ff0784c854baaa22d56918e2f2f6d09107
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-27 00:20:34 +0200

    [ADD] Import the Period entity to allow dependency injection in the service provider.

--- api/src/periods/periods.module.ts
+++ api/src/periods/periods.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PeriodsService } from './periods.service';
 import { PeriodsController } from './periods.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Period } from './entities/period.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Period])],
   controllers: [PeriodsController],
-  providers: [PeriodsService]
+  providers: [PeriodsService],
 })
 export class PeriodsModule {}
