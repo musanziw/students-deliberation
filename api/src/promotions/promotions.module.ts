968be9341c0a2275eb1da20b4d9d1d339cce1db8
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-27 00:22:01 +0200

    [ADD] Add promotion to allow dependency injection in the controller.

--- api/src/promotions/promotions.module.ts
+++ api/src/promotions/promotions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PromotionsService } from './promotions.service';
 import { PromotionsController } from './promotions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Promotion } from './entities/promotion.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Promotion])],
   controllers: [PromotionsController],
-  providers: [PromotionsService]
+  providers: [PromotionsService],
 })
 export class PromotionsModule {}
