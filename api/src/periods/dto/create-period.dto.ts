c7ccf5ba1d0c8415e4b2460f05c732d8b2c5a17a
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-28 00:45:28 +0200

    [DELETE] Remove unused rules.

--- api/src/periods/dto/create-period.dto.ts
+++ api/src/periods/dto/create-period.dto.ts
@@ -1,18 +1,12 @@
-import { IsDate, IsInt, MinDate } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class CreatePeriodDto {
   @IsInt({ message: 'Le semestre est requis.' })
   semester: number;
 
-  @IsDate({ message: 'La date de début est requise.' })
-  @MinDate(new Date(), {
-    message: 'La date de début doit être supérieure à la date actuelle.',
-  })
+  @IsNotEmpty({ message: 'La date de début est requise.' })
   started_at;
 
-  @IsDate({ message: 'La date de fin est requise.' })
-  @MinDate(() => new Date(), {
-    message: 'La date de fin doit être supérieure à la date actuelle.',
-  })
+  @IsNotEmpty({ message: 'La date de fin est requise.' })
   ended_at;
 }
