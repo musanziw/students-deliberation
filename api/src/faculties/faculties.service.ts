edbd9d925a68c2754e316b3c20a7aad622fb5ddd
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-28 00:43:03 +0200

    [REFACTOR] Remove unused code and optimization.

--- api/src/faculties/faculties.service.ts
+++ api/src/faculties/faculties.service.ts
@@ -1,9 +1,10 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateFacultyDto } from './dto/create-faculty.dto';
 import { UpdateFacultyDto } from './dto/update-faculty.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Faculty } from './entities/faculty.entity';
+import { UpdateResult } from 'typeorm/browser';
 
 @Injectable()
 export class FacultiesService {
@@ -17,32 +18,25 @@ export class FacultiesService {
       await this.facultyRepository.save(createFacultyDto);
       return {
         status: HttpStatus.CREATED,
-        message: 'La faculté créée avec succès.',
+        message: 'La faculté créée avec succès',
       };
     } catch {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: 'Impossible de créer la faculté.',
-      };
+      throw new HttpException(
+        'Impossible de créer la faculté',
+        HttpStatus.BAD_REQUEST,
+      );
     }
   }
 
   async findAll() {
-    try {
-      const faculties = await this.facultyRepository.find({
-        order: { id: 'ASC' },
-        relations: ['fields'],
-      });
-      return {
-        status: HttpStatus.OK,
-        faculties,
-      };
-    } catch {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: 'Impossible de récupérer les facultés.',
-      };
-    }
+    const faculties = await this.facultyRepository.find({
+      order: { id: 'ASC' },
+      relations: ['fields'],
+    });
+    return {
+      status: HttpStatus.OK,
+      faculties,
+    };
   }
 
   async findOne(id: number) {
@@ -55,40 +49,39 @@ export class FacultiesService {
         faculty,
       };
     } catch {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: 'Impossible de récupérer la faculté.',
-      };
+      throw new HttpException(
+        'Impossible de trouver la faculté',
+        HttpStatus.NOT_FOUND,
+      );
     }
   }
 
   async update(id: number, updateFacultyDto: UpdateFacultyDto) {
-    try {
-      await this.facultyRepository.update({ id }, updateFacultyDto);
-      return {
-        status: HttpStatus.OK,
-        message: 'La faculté a bien été mise à jour.',
-      };
-    } catch {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: 'Impossible de mettre à jour la faculté.',
-      };
-    }
+    const updateResult: UpdateResult = await this.facultyRepository.update(
+      { id },
+      updateFacultyDto,
+    );
+    if (!updateResult.affected)
+      throw new HttpException(
+        'Impossible de mettre à jour la faculté',
+        HttpStatus.NOT_FOUND,
+      );
+    return {
+      status: HttpStatus.OK,
+      message: 'La faculté a bien été mise à jour',
+    };
   }
 
   async remove(id: number) {
-    try {
-      await this.facultyRepository.delete({ id });
-      return {
-        status: HttpStatus.OK,
-        message: 'La faculté a bien été supprimée.',
-      };
-    } catch {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: 'Impossible de supprimer la faculté.',
-      };
-    }
+    const deleteResult = await this.facultyRepository.delete({ id });
+    if (!deleteResult.affected)
+      throw new HttpException(
+        'Impossible de supprimer la faculté',
+        HttpStatus.NOT_FOUND,
+      );
+    return {
+      status: HttpStatus.OK,
+      message: 'La faculté a bien été supprimée',
+    };
   }
 }
