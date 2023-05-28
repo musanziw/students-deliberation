f70ed483fd1bbe64a16807fb5b88a576bcc4b497
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-28 23:05:16 +0200

    [ADD] Add fields session, course, student.

--- api/src/grades/dto/create-grade.dto.ts
+++ api/src/grades/dto/create-grade.dto.ts
@@ -1,6 +1,17 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsObject, Max } from 'class-validator';
 
 export class CreateGradeDto {
-  @IsNumber({}, { message: 'La note annuelle est requise.' })
+  @IsNumber({}, { message: 'La note annuelle est requise' })
+  @Max(20, { message: 'La note annuelle doit être inférieure à 20' })
   average;
+
+  @IsNumber({}, { message: 'La session est requise' })
+  @Max(2, { message: 'La session doit être inférieure à 2' })
+  session;
+
+  @IsObject({ message: 'Le cours est requis' })
+  course: { id: number };
+
+  @IsObject({ message: "L'étudiant est requis" })
+  student: { id: number };
 }
