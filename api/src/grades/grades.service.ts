c5684dcf9dd695d07fadd23b7b96aab2a00b2532
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-28 23:07:31 +0200

    [ADD] Grade CURD.

--- api/src/grades/grades.service.ts
+++ api/src/grades/grades.service.ts
@@ -1,26 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateGradeDto } from './dto/create-grade.dto';
 import { UpdateGradeDto } from './dto/update-grade.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Grade } from './entities/grade.entity';
 
 @Injectable()
 export class GradesService {
-  create(createGradeDto: CreateGradeDto) {
-    return 'This action adds a new grade';
+  constructor(
+    @InjectRepository(Grade)
+    private gradeRepository: Repository<Grade>,
+  ) {}
+
+  async create(createGradeDto: CreateGradeDto) {
+    try {
+      await this.gradeRepository.save(createGradeDto);
+      return {
+        status: HttpStatus.CREATED,
+        message: 'La note a bien été créée',
+      };
+    } catch {
+      throw new HttpException(
+        'Impossible de créer la note',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all grades`;
+  async findAll() {
+    const grades = await this.gradeRepository.find({
+      order: { id: 'ASC' },
+      relations: ['student', 'course'],
+    });
+    return {
+      status: HttpStatus.OK,
+      grades,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} grade`;
+  async findOne(id: number) {
+    try {
+      const grade = await this.gradeRepository.findOneOrFail({
+        where: { id },
+        relations: ['student', 'course'],
+      });
+      return {
+        status: HttpStatus.OK,
+        grade,
+      };
+    } catch {
+      throw new HttpException(
+        'Impossible de trouver la note',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  update(id: number, updateGradeDto: UpdateGradeDto) {
-    return `This action updates a #${id} grade`;
+  async update(id: number, updateGradeDto: UpdateGradeDto) {
+    try {
+      const grade = await this.gradeRepository.findOneOrFail({
+        where: { id },
+      });
+      await this.gradeRepository.save({ ...grade, ...updateGradeDto });
+      return {
+        status: HttpStatus.OK,
+        message: 'La note a bien été mise à jour',
+      };
+    } catch {
+      throw new HttpException(
+        'Impossible de mettre à jour la note',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} grade`;
+  async remove(id: number) {
+    const deleteResult = await this.gradeRepository.delete({ id });
+    if (!deleteResult.affected) {
+      throw new HttpException(
+        'Impossible de supprimer la note',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return {
+      status: HttpStatus.OK,
+      message: 'La note a bien été supprimée',
+    };
   }
 }
