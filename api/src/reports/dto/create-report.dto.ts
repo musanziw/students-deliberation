c18c243dc3dc0ad38a6f4fd42bf7088b40d31148
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-28 23:02:12 +0200

    [DELETE] Remove unused.

--- api/src/reports/dto/create-report.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateReportDto {
-  @IsString({ message: 'La mention est requise.' })
-  mention;
-}
--- api/src/sessions/dto/create-session.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsNumber } from 'class-validator';
-
-export class CreateSessionDto {
-  @IsNumber({}, { message: 'Le niveau est requis.' })
-  level;
-}
