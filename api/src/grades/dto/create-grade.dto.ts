70d7ac1add1fdeefa36e9c74bf15269d1bfaaec9
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-30 16:53:15 +0200

    [UPDATE] Set session as an object.

--- api/src/grades/dto/create-grade.dto.ts
+++ api/src/grades/dto/create-grade.dto.ts
@@ -5,12 +5,11 @@ export class CreateGradeDto {
   @Max(20, { message: 'La note annuelle doit être inférieure à 20' })
   average;
 
-  @IsNumber({}, { message: 'La session est requise' })
-  @Max(2, { message: 'La session doit être inférieure à 2' })
-  session;
-
   @IsObject({ message: 'Le cours est requis' })
   course: { id: number };
+
+  @IsObject({ message: 'La session est requise' })
+  session: { id: number };
 
   @IsObject({ message: "L'étudiant est requis" })
   student: { id: number };
