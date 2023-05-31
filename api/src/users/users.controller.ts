281e48453143998e9e85594ed98036ed4b1a62ec
Wilfried Musanzi <musanziw.m@gmail.com>
2023-05-31 17:55:58 +0200

    [REMOVE] Remove profiles paths.

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -10,8 +10,6 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UpdateProfileDto } from './dto/update-profile.dto';
-import { UpdatePasswordDto } from './dto/update-password.dto';
 
 @Controller('users')
 export class UsersController {
@@ -35,27 +33,6 @@ export class UsersController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Get('profile')
-  profile(@Param('id') id: string) {
-    return this.usersService.profile(+id);
-  }
-
-  @Patch('update-profile')
-  updateProfile(
-    @Param('id') id: string,
-    @Body() updateProfileDto: UpdateProfileDto,
-  ) {
-    return this.usersService.updateProfile(+id, updateProfileDto);
-  }
-
-  @Patch('update-password')
-  updatePassword(
-    @Param('id') id: string,
-    @Body() updatePasswordDto: UpdatePasswordDto,
-  ) {
-    return this.usersService.updatePassword(+id, updatePasswordDto);
   }
 
   @Delete(':id')
