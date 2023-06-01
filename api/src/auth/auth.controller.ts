871a5a15d3861496e3b97ac7fd834d5063bf944f
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-01 17:45:41 +0200

    [ADD] Add login and profiles.

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,14 +1,34 @@
-import {Controller, Post} from '@nestjs/common';
-import {AuthService} from "./auth.service";
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signin.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { AuthGuard } from './guard/auth.guard';
+import { Public } from './guard/public.guard';
 
 @Controller()
+@UseGuards(AuthGuard)
 export class AuthController {
-    constructor(private authService: AuthService) {
-    }
+  constructor(private authService: AuthService) {}
 
-    @Post('login')
-    login() {
-        return this.authService.signin();
-    }
+  @Public()
+  @Post('login')
+  login(@Body() signinDto: SigninDto) {
+    return this.authService.signin(signinDto);
+  }
 
+  @Get('profile')
+  profile(@Req() req) {
+    return this.authService.profile(req);
+  }
+
+  @Post('update-profile')
+  updateProfile(@Req() req, @Body() updateProfileDto: UpdateProfileDto) {
+    return this.authService.updateProfile(req, updateProfileDto);
+  }
+
+  @Post('update-password')
+  updatePassword(@Req() req, @Body() updatePasswordDto: UpdatePasswordDto) {
+    return this.authService.updatePassword(req, updatePasswordDto);
+  }
 }
