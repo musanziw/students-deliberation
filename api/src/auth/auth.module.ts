34d48d1394c5d731be61413b2ed62bb0da259ff4
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-01 17:46:10 +0200

    [ADD] Import JWTService.

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './config/auth.constants';
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [AuthService],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
+    }),
+  ],
 })
 export class AuthModule {}
