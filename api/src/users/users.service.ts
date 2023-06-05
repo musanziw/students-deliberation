3f143b12af33f4095f35fbc18eafe8df6f850a18
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-05 18:25:43 +0200

    [ADD] Add user CRUD.

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,49 +1,92 @@
 import * as bcrypt from 'bcrypt';
-import { HttpStatus, Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { UpdateProfileDto } from '../auth/dto/update-profile.dto';
 import { CreateUserDto, UpdateUserDto } from './dto';
-import { UpdatePasswordDto } from '../auth/dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private config: ConfigService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private config: ConfigService,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
+    const salt: string = await bcrypt.genSalt(10);
+    const password: string = await bcrypt.hash(
+      this.config.get('DEFAULT_PASSWORD'),
+      salt,
+    );
     try {
-      const salt: string = await bcrypt.genSalt(10);
-      const password: string = await bcrypt.hash(
-        this.config.get('DEFAULT_PASSWORD'),
-        salt,
-      );
-      // await this.userRepository.save({
-      //   ...createUserDto,
-      //   password,
-      // });
+      await this.prismaService.user.create({
+        data: {
+          ...createUserDto,
+          password,
+          role: {
+            connect: createUserDto.role,
+          },
+        },
+      });
       return {
         status: HttpStatus.CREATED,
         message: "L'utilisateur a bien été créé",
       };
     } catch {
-      return {
-        status: HttpStatus.BAD_REQUEST,
-        message: "Erreur lors de la création de l'utilisateur",
-      };
+      throw new HttpException(
+        "Une erreur est survenue lors de la création de l'utilisateur",
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
-  async findAll() {}
+  async findAll() {
+    const users = await this.prismaService.user.findMany({
+      include: {
+        role: true,
+      },
+    });
+    return {
+      status: HttpStatus.OK,
+      users,
+    };
+  }
 
-  async findOne(id: number) {}
+  async findOne(id: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+      include: {
+        role: true,
+      },
+    });
+    return {
+      status: HttpStatus.OK,
+      user,
+    };
+  }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {}
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.prismaService.user.update({
+      where: { id },
+      data: {
+        ...updateUserDto,
+        role: {
+          set: updateUserDto.role,
+        },
+      },
+    });
+    return {
+      status: HttpStatus.OK,
+      message: "L'utilisateur a bien été modifié",
+    };
+  }
 
-  async remove(id: number) {}
-
-  async updateProfile(id: number, updateUserInfoDto: UpdateProfileDto) {}
-
-  async updatePassword(id: number, { newPassword }: UpdatePasswordDto) {}
-
-  async findByEmail(email: string) {}
+  async remove(id: number) {
+    await this.prismaService.user.delete({
+      where: { id },
+    });
+    return {
+      status: HttpStatus.OK,
+      message: "L'utilisateur a bien été supprimé",
+    };
+  }
 }
