af598d2d1e2d13b7402564ad827df03457e870ae
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-05 18:21:24 +0200

    [UPDATE] Use faculty id.

--- api/src/fields/dto/create-field.dto.ts
+++ api/src/fields/dto/create-field.dto.ts
@@ -1,9 +1,9 @@
-import { IsObject, IsString } from 'class-validator';
+import { IsNumber, IsObject, IsString } from 'class-validator';
 
 export class CreateFieldDto {
   @IsString({ message: 'Le nom est requis.' })
   name: string;
 
-  @IsObject({ message: 'La faculté est requise.' })
-  faculty: { id: number };
+  @IsNumber({}, { message: 'La faculté est requise.' })
+  faculty: number;
 }
