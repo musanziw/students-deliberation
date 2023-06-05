e14db22d8cdeddae105ff7a75975da8b10c18600
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-05 23:56:38 +0200

    [UPDATE] Migrate.

--- api/prisma/migrations/20230605142442_add_promotion_to_student/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `promotion` to the `students` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `students` ADD COLUMN `promotion` INTEGER NOT NULL;
