2047ca8ef8a2fc9824693049275b001830da2ee9
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-05 18:25:27 +0200

    [REMOVE] Remove properties.

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,17 +1,4 @@
-import { IsArray, IsString } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateUserDto {
-  @IsString({ message: 'Le prénom doit être une chaîne de caractères.' })
-  firstname: string;
-
-  @IsString({ message: 'Le nom doit être une chaîne de caractères.' })
-  lastname: string;
-
-  @IsString({
-    message: "Le nom d'utilisateur doit être une chaîne de caractères.",
-  })
-  name: string;
-
-  @IsArray({ message: 'Les rôles doivent être un tableau de rôles.' })
-  roles: any[];
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
