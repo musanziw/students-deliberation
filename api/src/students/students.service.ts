fcc90a54be2392d3e217c1b87a2269292e244053
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-05 22:19:00 +0200

    [ADD] Add students CRUD.

--- api/src/students/students.service.ts
+++ api/src/students/students.service.ts
@@ -1,19 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateStudentDto, UpdateStudentDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class StudentsService {
-  constructor() {}
+  constructor(private prismaService: PrismaService) {}
 
-  async create(createStudentDto: CreateStudentDto) {}
+  async create(createStudentDto: CreateStudentDto) {
+    try {
+      await this.prismaService.student.create({
+        data: {
+          ...createStudentDto,
+          field: {
+            connect: {
+              id: createStudentDto.field,
+            },
+          },
+        },
+      });
+      return {
+        status: HttpStatus.CREATED,
+        message: "L'étudiant a bien été créé",
+      };
+    } catch {
+      throw new HttpException(
+        'Un étudiant avec ces identifiants existe déjà',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-  async findAll() {}
+  async findAll() {
+    const students = await this.prismaService.student.findMany({
+      include: {
+        field: true,
+      },
+    });
+    return {
+      status: HttpStatus.OK,
+      students,
+    };
+  }
 
-  async findOne(id: number) {}
+  async findOne(id: number) {
+    const student = await this.prismaService.student.findUnique({
+      where: { id },
+      include: {
+        field: true,
+        courses: true,
+      },
+    });
+    return {
+      status: HttpStatus.OK,
+      student,
+    };
+  }
 
-  async update(id: number, updateStudentDto: UpdateStudentDto) {}
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+    try {
+      await this.prismaService.student.update({
+        where: { id },
+        data: {
+          ...updateStudentDto,
+          field: {
+            connect: {
+              id: updateStudentDto.field,
+            },
+          },
+        },
+      });
+      return {
+        status: HttpStatus.OK,
+        message: "L'étudiant a bien été modifié",
+      };
+    } catch {
+      throw new HttpException(
+        "Cette filière n'existe pas",
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-  async remove(id: number) {}
+  async remove(id: number) {
+    await this.prismaService.student.delete({
+      where: { id },
+    });
+    return {
+      status: HttpStatus.OK,
+      message: "L'étudiant a bien été supprimé",
+    };
+  }
 
   // calculatePercentage(grades: Grade[]) {
   //   const sum = grades.reduce(
