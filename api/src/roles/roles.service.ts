8a643617f0049f1cf48d85eaa8812d892595b7a8
Wilfried Musanzi <musanziw.m@gmail.com>
2023-06-05 18:24:26 +0200

    [ADD] Add user CRUD.

--- api/src/roles/roles.service.ts
+++ api/src/roles/roles.service.ts
@@ -1,17 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateRoleDto, UpdateRoleDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class RolesService {
-  constructor() {}
+  constructor(private prismaService: PrismaService) {}
 
-  async create(createRoleDto: CreateRoleDto) {}
+  async create(createRoleDto: CreateRoleDto) {
+    try {
+      await this.prismaService.role.create({
+        data: createRoleDto,
+      });
+      return {
+        status: HttpStatus.CREATED,
+        message: 'Le rôle a bien été créé',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Erreur lors de la création du rôle',
+      };
+    }
+  }
 
-  async findAll() {}
+  async findAll() {
+    try {
+      const roles = await this.prismaService.role.findMany({
+        orderBy: {
+          name: 'asc',
+        },
+      });
+      return {
+        status: HttpStatus.OK,
+        roles,
+      };
+    } catch {
+      throw new HttpException(
+        'Erreur lors de la récupération des rôles',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 
-  async findOne(id: number) {}
+  async findOne(id: number) {
+    try {
+      const role = await this.prismaService.role.findUniqueOrThrow({
+        where: { id },
+      });
+      return {
+        status: HttpStatus.OK,
+        role,
+      };
+    } catch {
+      throw new HttpException(
+        "Erreur lors de la récupération du rôle d'id " + id,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 
-  async update(id: number, updateRoleDto: UpdateRoleDto) {}
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    await this.prismaService.role.update({
+      where: { id },
+      data: updateRoleDto,
+    });
+    return {
+      status: HttpStatus.OK,
+      message: 'Le rôle a bien été mis à jour',
+    };
+  }
 
-  async remove(id: number) {}
+  async remove(id: number) {
+    try {
+      this.prismaService.role.delete({
+        where: { id },
+      });
+      return {
+        status: HttpStatus.OK,
+        message: 'Le rôle a bien été supprimé',
+      };
+    } catch {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Erreur lors de la suppression du rôle',
+      };
+    }
+  }
 }
