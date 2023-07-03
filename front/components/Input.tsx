5bcb17adbce55186c11920c6f9250a33976a552d
Wilfried Musanzi <musanziw.m@gmail.com>
2023-07-03 23:26:27 +0200

    [ADD] Import react.

--- front/components/Input.tsx
+++ front/components/Input.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface InputProps {
   type: string;
   name: string;
@@ -22,14 +24,13 @@ export default function Input({
                                 disabled,
                                 label,
                                 id,
-                                ...props
                               }: InputProps) {
   return (
     <div className="flex flex-col gap-y-2">
       <label htmlFor={id} className="text-md text-gray-900">{label}</label>
       <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} value={value}
-             className={"text-gray-500 rounded-sm border focus:ring-2 ring-blue-200 ring-offset-1 h-9 p-2 focus:outline-none focus:sh focus:inset-1 placeholder:text-sm"}
-             required={required} disabled={disabled} {...props} />
+             className={`text-gray-500 rounded-sm border focus:ring-2 ring-blue-200 ring-offset-1 h-9 p-2 focus:outline-none focus:sh focus:inset-1 placeholder:text-sm ${className}`}
+             required={required} disabled={disabled}/>
     </div>
   );
 }
