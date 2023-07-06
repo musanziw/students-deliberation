2751cd3f3a3efc60da1044232bf98de6fa27a963
Wilfried Musanzi <musanziw.m@gmail.com>
2023-07-06 23:42:12 +0200

    [REFACTOR] Remove unecessary.

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,23 +1,16 @@
 import "./globals.css";
 import React from "react";
 
-
 export const metadata = {
   title: "UNH - Déliberation",
   description: "UNH - Déliberation"
+
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <title>{metadata.title}</title>
-      <meta name="description" content={metadata.description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </head>
-    <body className={"font-inter"}>{children}</body>
+    <body className={"font-inter bg-gray-50 text-gray-600"}>{children}</body>
     </html>
   );
 }
