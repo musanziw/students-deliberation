6c82098c3f12cedec5c955807becefb7c381b6d5
Wilfried Musanzi <musanziw.m@gmail.com>
2023-07-06 23:41:22 +0200

    [ADD] Disable autocomplete.

--- front/components/Input.tsx
+++ front/components/Input.tsx
@@ -30,7 +30,7 @@ export default function Input({
       <label htmlFor={id} className="text-md text-gray-900">{label}</label>
       <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} value={value}
              className={`text-gray-500 rounded-sm border focus:ring-2 ring-blue-200 ring-offset-1 h-9 p-2 focus:outline-none focus:sh focus:inset-1 placeholder:text-sm ${className}`}
-             required={required} disabled={disabled}/>
+             required={required} disabled={disabled} autoComplete={'off'} />
     </div>
   );
 }
