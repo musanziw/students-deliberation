e618e4409258494305e4d5ab8a63a26b29861778
Wilfried Musanzi <musanziw.m@gmail.com>
2023-07-08 13:03:10 +0200

    [REMOVE] Remove rounded-sm.

--- front/components/Input.tsx
+++ front/components/Input.tsx
@@ -23,14 +23,14 @@ export default function Input({
                                 required,
                                 disabled,
                                 label,
-                                id,
+                                id
                               }: InputProps) {
   return (
     <div className="flex flex-col gap-y-2">
       <label htmlFor={id} className="text-md text-gray-900">{label}</label>
       <input type={type} name={name} id={id} placeholder={placeholder} onChange={onChange} value={value}
-             className={`text-gray-500 rounded-sm border focus:ring-2 ring-blue-200 ring-offset-1 h-9 p-2 focus:outline-none focus:sh focus:inset-1 placeholder:text-sm ${className}`}
-             required={required} disabled={disabled} autoComplete={'off'} />
+             className={`text-gray-500 border focus:ring-2 ring-blue-200 ring-offset-1 h-9 p-2 focus:outline-none focus:sh focus:inset-1 placeholder:text-sm ${className}`}
+             required={required} disabled={disabled} autoComplete={"off"} />
     </div>
   );
 }
